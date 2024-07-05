441413959081f6f678b4a50476fd0e05b6ba3994
sl <s4210110@163.com>
2024-07-05 08:17:22 +0800

    release: @react-native-oh-tpl/react-native-sensitive-info@6.0.0-alpha.9-0.0.1

---
+++ harmony/react_native_sensitive_info/src/main/ets/tools.ts
@@ -0,0 +1,54 @@
+import cryptoFramework from '@ohos.security.cryptoFramework';
+import userAuth from '@ohos.userIAM.userAuth';
+
+
+export class tool {
+
+
+  widgetParam: userAuth.WidgetParam = {
+    title: '请输入密码',
+  };
+
+  authParam: userAuth.AuthParam = {
+    challenge: new Uint8Array([49, 49, 49, 49, 49, 49]),
+    authType: [userAuth.UserAuthType.FINGERPRINT],
+    authTrustLevel: userAuth.AuthTrustLevel.ATL1,
+  };
+
+  genIvParamsSpec() {
+    let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // 16 bytes
+    let dataIv = new Uint8Array(arr);
+    let ivBlob: cryptoFramework.DataBlob = { data: dataIv };
+    let ivParamsSpec: cryptoFramework.IvParamsSpec = {
+      algName: "IvParamsSpec",
+      iv: ivBlob
+    };
+    return ivParamsSpec;
+  }
+
+  // 分段加密消息
+  async encryptMessagePromise(symKey: cryptoFramework.SymKey, plainText: cryptoFramework.DataBlob) {
+    let cipher = cryptoFramework.createCipher('AES128|CBC|PKCS7');
+    let iv = this.genIvParamsSpec();
+    await cipher.init(cryptoFramework.CryptoMode.ENCRYPT_MODE, symKey, iv);
+    let cipherData = await cipher.doFinal(plainText);
+    return cipherData;
+  }
+
+  // 分段解密消息
+  async  decryptMessagePromise(symKey: cryptoFramework.SymKey, cipherText: cryptoFramework.DataBlob) {
+    let decoder = cryptoFramework.createCipher('AES128|CBC|PKCS7');
+    let iv = this.genIvParamsSpec();
+    await decoder.init(cryptoFramework.CryptoMode.DECRYPT_MODE, symKey, iv);
+    let decryptData = await decoder.doFinal(cipherText);
+    return decryptData;
+  }
+
+  async  genSymKeyByData(symKeyData: Uint8Array) {
+    let symKeyBlob: cryptoFramework.DataBlob = { data: symKeyData };
+    let aesGenerator = cryptoFramework.createSymKeyGenerator('AES128');
+    let symKey = await aesGenerator.convertKey(symKeyBlob);
+    console.info('convertKey success');
+    return symKey;
+  }
+}
