35aa9eb7b21143ee3c1a8a93162649a88d82fa90
yangzhipeng-xx <140054030+yangzhipeng-xx@users.noreply.github.com>
2024-12-17 18:38:30 +0800

    chore: add license header and format code style (#18)
    Co-authored-by: yangzhipeng <yangzhipeng27@h-partners.com>

--- harmony/react_native_sensitive_info/src/main/ets/tools.ts
+++ harmony/react_native_sensitive_info/src/main/ets/tools.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import cryptoFramework from '@ohos.security.cryptoFramework';
 import userAuth from '@ohos.userIAM.userAuth';
 
